6a202e6defd8dec0d81ae490338f9f31bb98ae06
Rohit S Motammanavar <rsmottomanavar@gmail.com>
2026-03-30 19:14:09 +0530

    Update input_output.py

--- input_output.py
+++ input_output.py
@@ -1,5 +1,5 @@
-'''name = input("What is your name ? ")
-print("Hello " + name)'''
+name = input("What is your name ? ")
+print("Hello " + name)
 
 name = input("What is your name ? ")
 age = input("What is your age ? ")
