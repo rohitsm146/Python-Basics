11ed514b83148701e6c259675750cc370e8f58d7
Rohit S Motammanavar <rsmottomanavar@gmail.com>
2026-04-04 10:25:01 +0530

    Add files via upload

---
+++ ex4print.py
@@ -0,0 +1,17 @@
+#user name and age
+"""user_name = input("Enter your name : ")
+brith_year = int(input("Enter your birth year : "))
+age = 2026 - brith_year
+print(f"Hello {user_name}, You are age is {age} ")"""
+
+# Unit Converter
+temp_celsius = float(input("Enter temperature in Celsius: "))
+
+temp_fahrenheit = (temp_celsius * 9/5) + 32
+temp_kelvin = temp_celsius + 273.15
+
+print("="*10)
+print(f"Fahrenheit = {temp_fahrenheit}")
+print("="*10)
+print(f"Kelvin = {temp_kelvin}")
+print("="*10)
---
+++ print.py
@@ -0,0 +1,73 @@
+# print("Hello, World")
+# print(10)
+# print(2 + 3)
+# print("Python is powerful")
+
+# print("Python", "Java", "C++")
+# print("Python", "Java", "C++", sep=", ")
+# print("Python", "Java", "C++", sep=" | ")
+
+# print("Loading", end="...")
+# print(" Done!")
+
+# with open("log.txt", "w") as f:
+#     print("App started", file=f)
+#     print("User logged in", file=f)
+
+
+# import time
+
+# print("Step 1...", flush=True)
+# time.sleep(1)
+# print("Step 2...", flush=True)
+# time.sleep(1)
+# print()
+# print("Done!", flush=True)
+
+# print("=" * 40)
+# print("""
+# ==========================
+#    Welcome to Python!
+# ==========================
+# """)
+
+# text = "Hello\tWorld\n"
+
+# print(repr(text))
+
+# price = input("Enter price: ")
+# print(f"DEBUG: price = {price}")
+# print(f"DEBUG: type = {type(price)}")
+# discount = 20
+
+# final = price - (price * discount / 100)
+# print(f"Final price: {final}")
+
+# name = input("Enter your name: ")
+# print("Hello,", name)
+
+# text = "Hello\tWorld\n"
+# print(repr(text))
+# print(text)
+
+# name = input("Enter your name: ")
+# print("Hello,", name)
+
+# age = int(input("Enter your age: "))
+# print(type(age))
+
+# import sys
+# # Run as: python greet.py OnePercentDev
+# name = sys.argv[1]
+# print(f"Hello, {name}")
+
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+
+# print("Sum:", a + b)
+# print("Difference:", a - b)
+# print("Product:", a * b)
+
+name = "OnePercentDev"
+age = 21
+print(f"My name is {name} and I am {age} years old")
